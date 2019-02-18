2bdaeefe47f44bde61d8793cc5d9ee5d23719f3d
bgcert <info@seminari365.com>
2019-02-18 18:35:10 +0200

    First commit again.

---
+++ resources/views/services.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Services</h1>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('homepage');
+    // return view('home');
+});
+
+Route::view('/services', 'services');
+Route::view('/courses', 'courses');
+Route::view('/contacts', 'contacts');
+Route::view('/aboutus', 'aboutus');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

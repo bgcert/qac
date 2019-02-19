c1dce7cadcdefc3445064523051f94d4d6fcd80d
bgcert <info@seminari365.com>
2019-02-19 16:52:58 +0200

    Homepage center icon circles.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -17,7 +17,7 @@
         </div>
         <div class="row services">
             <div class="service col-4">
-                <div>
+                <div class="service__icon">
                     <div class="circle">
                         <ion-icon name="help-buoy"></ion-icon>
                     </div>
@@ -27,7 +27,7 @@
                 </div>
             </div>
             <div class="service col-4">
-                <div>
+                <div class="service__icon">
                     <div class="circle">
                         <ion-icon name="ribbon"></ion-icon>
                     </div>
@@ -37,46 +37,50 @@
                 </div>
             </div>
             <div class="service col-4">
-                <div>
+                <div class="service__icon">
                     <div class="circle">
                         <ion-icon name="pricetag"></ion-icon>
                     </div>
-                    <ion-icon name="pricetag"></ion-icon>
                 </div>
                 <div class="title">
                 Маркировка СЕ
                 </div>
             </div>
             <div class="service col-4">
-                <div>
+                <div class="service__icon">
                     <div class="circle">
                         <ion-icon name="wallet"></ion-icon>
                     </div>
-                    <ion-icon name="wallet"></ion-icon>
                 </div>
                 <div class="title">
                 Европейски фондове
                 </div>
             </div>
             <div class="service col-4">
-                <div>
-                    <ion-icon name="information-circle-outline"></ion-icon>
+                <div class="service__icon">
+                    <div class="circle">
+                        <ion-icon name="information-circle-outline"></ion-icon>
+                    </div>
                 </div>
                 <div class="title">
                     Информация
                 </div>
             </div>
             <div class="service col-4">
-                <div>
-                    <ion-icon name="book"></ion-icon>
+                <div class="service__icon">
+                    <div class="circle">
+                        <ion-icon name="book"></ion-icon>
+                    </div>
                 </div>
                 <div class="title">
                 Обучение
                 </div>
             </div>
             <div class="service col-4">
-                <div>
-                    <ion-icon name="document"></ion-icon>
+                <div class="service__icon">
+                    <div class="circle">
+                        <ion-icon name="document"></ion-icon>
+                    </div>
                 </div>
                 <div class="title">
                 Консултации и подготовка на документи и защита при проверки от "Надзор на пазара" и КЗП

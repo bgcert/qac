c74d3efc5a244a604e312eec16463180424264c0
bgcert <info@seminari365.com>
2019-03-13 17:50:28 +0200

    Redesign again.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -20,7 +20,180 @@
 
         </div>
     </div>
+</section>
 
+<section class="home-links">
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <h5 class="card-title">Маркировка CE</h5>
+            </div>
+            <div class="col-4">
+                <h5 class="card-title">Внедряване на международни стандарти</h5>
+            </div>
+            <div class="col-4">
+                <h5 class="card-title">Кандидатстване за безвъзмездно финансиране</h5>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<section class="services">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-5 mr-auto">
+                <img src="/img/inspection.jpeg" class="rounded img-fluid" alt="">
+            </div>
+            <div class="col-6 d-flex align-items-center">
+                <div>
+                    <div class="h1">
+                        Постигане на съответствие
+                    </div>
+                    <p>
+                        Маркировка CE и сертификация на продуктите за европейския и международен пазар
+                    </p>
+                    <p>
+                        <a href="#">Научете повече &rarr;</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section class="standarts">
+
+    <div class="container">
+        <div class="text-center">
+            <div class="h1">
+                Системи за управление на бизнеса
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-4">
+                <div class="h4">
+                    Изграждане и внедряване
+                </div>
+
+                <p> на системи за управление по международни стандарти</p>
+            </div>
+
+            <div class="col-4">
+                <div class="h4">
+                    Поддържане и развитие
+                </div>
+
+                <p> бизнесът Ви има нужда от ъпгрейд</p>
+            </div>
+
+            <div class="col-4">
+                <div class="h4">
+                    Публикации на НАТО
+                </div>
+
+                <p>международни стандарти и публикации на НАТО</p>
+            </div>
+        </div>
+
+        <div class="standarts__list">
+            <div class="row">
+                <div class="col-3">
+                    <div class="row my-4">
+                        <div class="col-md-4">
+                            <img src="/img/3.jpeg" class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <p class="card-text">ISO 9001:2008</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-3">
+                    <div class="row my-4">
+                        <div class="col-md-4">
+                            <img src="/img/3.jpeg" class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <p class="card-text">BS OHSAS 18001:2007</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-3">
+                    <div class="row my-4">
+                        <div class="col-md-4">
+                            <img src="/img/3.jpeg" class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <p class="card-text">ISO 14001:2004</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-3">
+                    <div class="row my-4">
+                        <div class="col-md-4">
+                            <img src="/img/3.jpeg" class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <p class="card-text">Серия ISO/IEC 17000</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-3">
+                    <div class="row my-4">
+                        <div class="col-md-4">
+                            <img src="/img/3.jpeg" class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <p class="card-text">ISO 9001:2008</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-3">
+                    <div class="row my-4">
+                        <div class="col-md-4">
+                            <img src="/img/3.jpeg" class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <p class="card-text">BS OHSAS 18001:2007</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-3">
+                    <div class="row my-4">
+                        <div class="col-md-4">
+                            <img src="/img/3.jpeg" class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <p class="card-text">ISO 14001:2004</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section class="funding">
+    <div class="container">
+        <div class="cover">
+            <div>
+                <h1>Безвъзмездно финансиране</h1>
+                <p>Подготовка за Кандидатстване по европейски програми</p>
+                <p><a href="#" class="btn btn-light btn-lg">Научете повече</a></p>
+            </div>
+        </div>
+    </div>
 </section>
 
 <section class="about-us">
@@ -41,84 +214,19 @@
 
 </section>
 
-<section class="services">
-
-    <div class="container">
-        <div class="row">
-            <div class="col-5 mr-auto">
-                <img src="/img/inspection.jpeg" class="rounded img-fluid" alt="">
-            </div>
-            <div class="col-6 d-flex align-items-center">
-                <div>
-                    <div class="h1">
-                        Оценка на съответствието на продуктите
-                    </div>
-                    <p>
-                        По изискванията на европейските директиви и приложими стандарти за постигане на <strong>маркировка СЕ</strong>
-                    </p>
-                    <p>
-                        <a href="#">Научете повече &rarr;</a>
-                    </p>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-6 d-flex align-items-center">
-                <div>
-                    <div class="h1">
-                        Управление на проекти
-                    </div>
-                    <p>
-                        За инвестиции в предприятия по европейски и международни фондове и програми
-                    </p>
-                    <p>
-                        <a href="#">Научете повече &rarr;</a>
-                    </p>
-                </div>
-            </div>
-            <div class="col-5 ml-auto">
-                <img src="/img/money.jpeg" class="rounded img-fluid" alt="">
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-5 mr-auto">
-                <img src="/img/globe.jpeg" class="rounded img-fluid" alt="">
-            </div>
-            <div class="col-6 d-flex align-items-center">
-                <div>
-                    <div class="h1">
-                        Експертна подкрепа и бизнес консултации
-                    </div>
-                    <p>
-                        Внедряване на международни и европейски стандарти (ISO, EN...) за качество, безопасност на храни, околна среда, ЗБУТ и др.
-                        и усъвършенстване на процесите във фирмите
-                    </p>
-                    <p>
-                        <a href="#">Научете повече &rarr;</a>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-
-</section>
-
 <section class="clients">
 
     <div class="container">
         <div class="row">
 
-            @for($i = 0; $i <= 20; $i++)
-            <div class="col-2 client">
+            @for($i = 0; $i <= 20; $i++) <div class="col-2 client">
                 <a href="#">
                     <img src="/img/dummy-logo.png" class="img-fluid" alt="">
                 </a>
-            </div>
-            @endfor
-
         </div>
+        @endfor
+
+    </div>
 
     </div>
 
@@ -137,8 +245,7 @@
                     По-добра оценка за нашата работа не може да има.
                 </p>
             </div>
-            @for($i = 0; $i <= 6; $i++)
-            <div class="col-6 p-2">
+            @for($i = 0; $i <= 6; $i++) <div class="col-6 p-2">
                 <blockquote class="blockquote h-100 reference-box">
                     <p>
                         <img src="/img/dummy-logo.png" alt="Eлeктрон Прогрес АД - София">
@@ -148,9 +255,9 @@
                     </p>
                     <footer class="blockquote-footer">Eлeктрон Прогрес АД - София</footer>
                 </blockquote>
-            </div>
-            @endfor
         </div>
+        @endfor
+    </div>
     </div>
 
 </section>
@@ -158,7 +265,9 @@
 <section class="contact-link">
     <div class="container text-center">
         <div class="h3">
-            Имате нова визия за бъдещето на своя бизнес? Ние ще Ви помогнем да я реализирате.
+            Имате нова визия за бъдещето на своя бизнес?
+            <br>
+            Ние ще Ви помогнем да я реализирате.
         </div>
         <p>
             <a href="#" class="btn btn-primary btn-lg">Свържете се с нас</a>

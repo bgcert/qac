026ad04434569076870a0b5870f74eea399bcf62
bgcert <info@seminari365.com>
2019-03-18 18:21:21 +0200

    Improvements n++

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,415 +3,185 @@
 @section('content')
 
 <section class="slider">
-    <div class="overlay">
-        <div class="container">
-            <div class="h1">
-                <strong>Кю Ей Си ООД</strong> - Център за осигуряване на качестваото
-            </div>
-
-            <div class="h3">
-                Повишете конкурентността на Вашия бизнес
-            </div>
-
-            <p>
-                <a href="#" class="btn btn-lg btn-warning">Услуги</a>
-                <a href="#" class="btn btn-link">Контакти</a>
-            </p>
-
+  <div class="container">
+      <div class="row">
+        <div class="col-4">
+            <img src="/img/logo-grey-transparent.png" alt="">
         </div>
-    </div>
-</section>
-
-<section class="intro">
-    <div class="container">
-
-        <div class="d-flex justify-content-center">
-            <div class="col-7">
-                <div class="h1">
-                    Как можем да помогнем на Вашия бизнес
-                </div>
-                <p>
-                    От 1997 г. до днес компанията предлага специализирани консултантски услуги и обучение в специфичните области на сертификацията и управлението.
-                </p>
-                <p>
-                    <a href="#" class="btn btn-warning">Научете повече</a>
-                </p>
-            </div>
-        </div>
-
-    </div>
-
+      </div>
+    <h1 class="display-6">Съдействие за успешен бизнес</h1>
+    <p class="lead">От 1997 г. до днес компанията предлага специализирани консултантски услуги и обучение в специфичните области на сертификацията и управлението.</p>
+  </div>
 </section>
 
 <section class="main-services">
-
     <div class="container">
-        <div class="divider">
-            <div class="line"></div>
-        </div>
-
-        <h4>Основни направления</h4>
-        <div class="row">
-            <div class="col-6">
-                <div class="q-card" style="background: url('/img/2.jpeg');">
-
-                    <div class="body">
-                        <h4>Внедряване на системи за управление</h4>
-                        <p>
-                            <a href="/certification" class="btn btn-primary btn-sm">Научете повече &rarr;</a>
-                        </p>
-                    </div>
-
+        <div class="d-flex justify-content-center text-center">
+            <div class="col-9">
+                <div class="h1 service-lead">
+                    Над 20 годишен опит в консултациите и обученията
                 </div>
+                <p class="service-content">
+                    От 1997 г. до днес компанията предлага специализирани консултантски услуги и
+                    обучение в специфичните области на сертификацията и управлението.
+                </p>
             </div>
+        </div>
+        <div class="h4 service-title">
+            Основни направления
+        </div>
+        <div class="row no-gutters">
             <div class="col-6">
-                <div class="q-card" style="background: url('/img/4.jpeg');">
-                    <div class="body">
-                        <h4>Продуктова безопасност и маркировка CE</h4>
-                        <p>
-                            <a href="#" class="btn btn-primary btn-sm">Научете повече &rarr;</a>
-                        </p>
+                <div class="service-box" style="background: url('/img/homepage1.jpeg');">
+                    <div class="overlay">
+                        <div class="h4">Внедряване на системи за управление</div>
                     </div>
                 </div>
             </div>
+            <div class="col-6">
+                <div class="service-box" style="background: url('/img/safety.jpg');">
+                    <div class="overlay">
+                        <div class="h4">Продуктова безопасност</div>
+                    </div>
+                </div>
+            </div>
         </div>
+        <br>
+        <br>
+        <hr>
     </div>
-
 </section>
 
-<div class="container">
-    <section class="standarts">
-
-        <div class="container">
-            <div class="divider">
-                <div class="line"></div>
-            </div>
-            <div class="text-center">
-                <div class="h1">
-                    Системи за управление на бизнеса
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-4">
-                    <div class="h4">
-                        Изграждане и внедряване
-                    </div>
-
-                    <p> на системи за управление по международни стандарти</p>
-                </div>
-
-                <div class="col-4">
-                    <div class="h4">
-                        Поддържане и развитие
-                    </div>
-
-                    <p> бизнесът Ви има нужда от ъпгрейд</p>
-                </div>
-
-                <div class="col-4">
-                    <div class="h4">
-                        Публикации на НАТО
-                    </div>
-
-                    <p>международни стандарти и публикации на НАТО</p>
-                </div>
-            </div>
-
-            <div class="text-center call-to-action">
-                <a href="#" class="btn btn-lg btn-warning">Какво представляват системите?</a>
-            </div>
-
-            <!-- <div class="divider">
-                <div class="line"></div>
-            </div> -->
-        </div>
-
-    </section>
-    <h3>
-        Системите, които внедряваме
-    </h3>
-    <div class="row">
-        <div class="col-4">
-            <div class="standart-box">
-                <div class="row">
-                    <div class="col-4">
-                        <img src="/img/iso9001.jpg" class="img-fluid" alt="">
-                    </div>
-                    <div class="col-8">
-                        Управление на качеството
-                        <p>
-                            <strong>ISO 9001</strong>
-                        </p>
-                        <a href="/standarts/iso-9001" class="btn btn-sm btn-danger">Научете повече</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-4">
-            <div class="row">
-                <div class="col-4">
-                    <img src="/img/iso9001.jpg" class="img-fluid" alt="">
-                </div>
-                <div class="col-8">
-                    Информационна сигурност
-                    <p>
-                        <strong>ISO/IEC 27001</strong>
-                    </p>
-                    <a href="/standarts/iso-27001" class="btn btn-sm btn-danger">Научете повече</a>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-4">
-            Информационна сигурност
-            <p>
-                <strong>ISO/IEC 27001</strong>
-            </p>
-            <a href="#" class="btn btn-danger">Научете повече</a>
-        </div>
-        <div class="col-4">
-            Медицински инструменти
-            <p>
-                <strong>ISO 13485</strong>
-            </p>
-            <a href="#" class="btn btn-danger">Научете повече</a>
-        </div>
-        <div class="col-4 mt-4">
-            Информационна сигурност
-            <p>
-                <strong>ISO/IEC 27001</strong>
-            </p>
-            <a href="#" class="btn btn-danger">Научете повече</a>
-        </div>
-        <div class="col-4 mt-4">
-            Медицински инструменти
-            <p>
-                <strong>ISO 13485</strong>
-            </p>
-            <a href="#" class="btn btn-danger">Научете повече</a>
+<section class="contact-link">
+    <div class="container">
+        <div class="d-flex align-items-center justify-content-between">
+            <div class="h4">Имате допълнителни въпроси?</div>
+            <a href="#" class="btn btn-outline-primary">Свържете се с нас</a>
         </div>
     </div>
-</div>
+</section>
 
-<hr>
+<section class="standarts">
+    <div class="container">
+        <div class="h3 title">
+            Стандартите, които внедряваме
+        </div>
+        <div class="row no-gutters">
+            <div class="col-4">
+                <div class="standart-box" style="background-image: url('/img/iso9001.jpg');">
+                    <div class="content">
+                        <div class="h3">ISO 9001</div>
+                        <p>Системи за управление на качеството</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="standart-box" style="background-image: url('/img/environment.jpg');">
+                    <div class="content">
+                        <div class="h3">ISO 14001</div>
+                        <p>Управление на околната среда</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="standart-box" style="background-image: url('/img/ohsas.jpg');">
+                    <div class="content">
+                        <div class="h3">OHSAS 18001</div>
+                        <p>Безопасни условия на труд</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
+<br><br><br>
 
-
-<!-- <section class="home-links">
+<section class="product-certification">
     <div class="container">
         <div class="row">
-            <div class="col-4">
-                <h5 class="card-title">Маркировка CE</h5>
-            </div>
-            <div class="col-4">
-                <h5 class="card-title">Внедряване на международни стандарти</h5>
-            </div>
-            <div class="col-4">
-                <h5 class="card-title">Кандидатстване за безвъзмездно финансиране</h5>
-            </div>
-        </div>
-
-    </div>
-
-</section> -->
-
-<section class="services">
-    <div class="divider">
-        <div class="line"></div>
-    </div>
-    <div class="container">
-        <div class="row announce">
-            <div class="col-5 mr-auto">
-                <img src="/img/inspection.jpeg" class="rounded img-fluid" alt="">
+            <div class="col-6">
+            
             </div>
             <div class="col-6 d-flex align-items-center">
                 <div>
-                    <div class="h1">
-                        Постигане на съответствие
+                    <div class="h3">
+                        Продуктова безопасност и маркировка CE
                     </div>
                     <p>
-                        Маркировка CE и сертификация на продуктите за европейския и международен пазар
+                        Как можем да Ви съдействаме:
                     </p>
+                    <ul>
+                        <li>
+                            Проверка на изискванията конкретно за Вашия продукт
+                        </li>
+                        <li>
+                            Идентифициране на приложимите стандарти и директиви
+                        </li>
+                        <li>
+                            Оценка на продукта и преглед на съответствието с приложимите изисквания.
+                        </li>
+                    </ul>
                     <p>
-                        <a href="#">Научете повече &rarr;</a>
+                        <a href="#">Научете повече</a>
                     </p>
                 </div>
-            </div>
-        </div>
-
-        <div class="row sectors">
-            <div class="col-6">
-                <div class="h3">
-                    Заинтересовани сектори
-                </div>
-                <p>
-                    Изпълнение на изискванията на европейски регламенти и директиви за свободно движение на стоки
-                </p>
-            </div>
-            <div class="col-3">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><a href="#">&rarr; Cras justo odio</a></li>
-                    <li class="list-group-item">Dapibus ac facilisis in</li>
-                    <li class="list-group-item">Morbi leo risus</li>
-                    <li class="list-group-item">Porta ac consectetur ac</li>
-                    <li class="list-group-item">Vestibulum at eros</li>
-                </ul>
-            </div>
-            <div class="col-3">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Cras justo odio</li>
-                    <li class="list-group-item">Dapibus ac facilisis in</li>
-                    <li class="list-group-item">Morbi leo risus</li>
-                    <li class="list-group-item">Porta ac consectetur ac</li>
-                    <li class="list-group-item">Vestibulum at eros</li>
-                </ul>
-            </div>
-        </div>
-        <hr>
-        <div class="text-center mb-4">
-            <div class="h1">
-                Как можем да Ви съдействаме
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-6">
-                <h3>
-                    Консултации
-                </h3>
-                <p>
-                    Кю Ей Си ООД извършва консултации и по целия процес за придобиване на маркировката
-                </p>
-                <p>
-                    <a href="#">Изпратете запитване &rarr;</a>
-                </p>
-            </div>
-
-            <div class="col-6">
-                <h3>Обучение</h3>
-                <p>
-                    Фирмата предлага обучения по процедурите за изискванията на ЕС ....
-                </p>
-                <p>
-                    <a href="#">Вижте актуалните ни курсове &rarr;</a>
-                </p>
             </div>
         </div>
     </div>
-
 </section>
 
-
-
-<section class="funding">
+<section class="courses">
     <div class="container">
-        <div class="divider">
-            <div class="line"></div>
-        </div>
-        <div class="cover">
-            <div>
-                <h1>Безвъзмездно финансиране</h1>
-                <p>Подготовка за Кандидатстване по европейски програми</p>
-                <p><a href="#" class="btn btn-light btn-lg">Разгледайте възможностите</a></p>
+        <div class="row">
+            <div class="col-5">
+                <div class="h4">
+                    Споделен опит
+                </div>
+                <p>
+                    Кю Ей Си" ООД предлага динамично интерактивно обучение на специалисти от фирмите с пряка практическа насоченост.
+                </p>
+            </div>
+            <div class="col-7">
+                <div class="row">
+                    <div class="col-6">
+                        <p>img</p>
+                        <p>title</p>
+                    </div>
+                    <div class="col-6">
+                        <p>img</p>
+                        <p>title</p>
+                    </div>
+                </div>
+                <a href="#" class="btn btn-primary">Всички обучения</a>
             </div>
         </div>
+
+    </div>
+</section>
+
+<section class="clients">
+    <div class="container">
+        <div class="text-center">
+            Нашите клиенти
+        </div>
+        <ul>
+            <li>Лого 1</li>
+            <li>Лого 2</li>
+        </ul>
     </div>
 </section>
 
 <section class="about-us">
-
-    <div class="container">
-        <div class="divider">
-            <div class="line"></div>
-        </div>
-        <div class="text-center">
-            <div class="title">
-                За нас
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-6">
-                <h3>Качеството - реално бъдеще</h3>
-                <p>
-                    От 1997 г. до днес компанията предлага специализирани консултантски услуги и обучение в специфичните области на сертификацията и управлението.
-                </p>
-                <p>
-                    <a href="#">Научете повече за фирмата &rarr;</a>
-                </p>
-            </div>
-            <div class="col-6">
-                <h3>Бизнес консултации</h3>
-                <p>
-                    От 2001 година, всички предлагани услуги са обхванати от сертифицираната Система за управление на качеството по изискванията на <strong>ISO 9001:2015</strong>
-                </p>
-                <p>
-                    <a href="#">Вижте сертификата &rarr;</a>
-                </p>
-            </div>
-        </div>
-    </div>
-
-</section>
-
-<section class="clients">
-
-    <div class="container">
-        <div class="row">
-
-            @for($i = 0; $i <= 20; $i++) <div class="col-2 client">
-                <a href="#">
-                    <img src="/img/dummy-logo.png" class="img-fluid" alt="">
-                </a>
-        </div>
-        @endfor
-
-    </div>
-
-    </div>
-
-</section>
-
-<section class="references">
-
-    <div class="container">
-        <div class="row">
-            <div class="col-8">
-                <h2>
-                    Как ни оценяват клиентите ни
-                </h2>
-                <p class="header">
-                    За нас фактът че бивши клиенти се обръщат пак към нас за следваща консултация, означава много.
-                    По-добра оценка за нашата работа не може да има.
-                </p>
-            </div>
-            @for($i = 0; $i <= 6; $i++) <div class="col-6 p-2">
-                <blockquote class="blockquote h-100 reference-box">
-                    <p>
-                        <img src="/img/dummy-logo.png" alt="Eлeктрон Прогрес АД - София">
-                    </p>
-                    <p>
-                        ... За краткия период на курсовете за обучение водещите успяха да потопят всички участници в изключително приятна и ползотворна работна атмосфера, в условията на която под формата на игри и симулиране на ситуации, обучаемите успяха да систематизират и усвоят теоретичен материал, касаещ общуването, конфликтните ситуации и евентуален подход към тях при внедряването, поддържането, развитието на системата за управление и провеждането на вътрешни одитори ....
-                    </p>
-                    <footer class="blockquote-footer">Eлeктрон Прогрес АД - София</footer>
-                </blockquote>
-        </div>
-        @endfor
-    </div>
-    </div>
-
-</section>
-
-<section class="contact-link">
     <div class="container text-center">
-        <div class="h3">
-            Имате нова визия за бъдещето на своя бизнес?
-            <br>
-            Ние ще Ви помогнем да я реализирате.
+        <div class="h1">
+            За нас
         </div>
         <p>
-            <a href="#" class="btn btn-primary btn-lg">Свържете се с нас</a>
+            Някво инфо
         </p>
+        <a href="#" class="btn btn-primary">Контакти</a>
     </div>
 </section>
+
 @endsection 

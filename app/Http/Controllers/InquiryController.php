ccbd5978f760c39ed9bf207855c4a86f387a3cc5
bgcert <info@seminari365.com>
2019-05-07 13:55:02 +0300

    Inquiry mail test.

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Mail;
 use App\Inquiry;
 use function GuzzleHttp\json_decode;
 
@@ -11,15 +12,17 @@ class InquiryController extends Controller
     public static function inquiryTypes()
     {
         return [
-                    1 => 'Внедряване на Система за управление',
-                    2 => 'Изпълнение на изисквания на НАТО',
-                    3 => 'Продуктово съответствие и СЕ маркировка',
-                    4 => 'Друг интерес'
-                ];
+            1 => 'Внедряване на Система за управление',
+            2 => 'Изпълнение на изисквания на НАТО',
+            3 => 'Продуктово съответствие и СЕ маркировка',
+            4 => 'Друг интерес'
+        ];
     }
     public function store()
     {
         Inquiry::create(request()->all());
+        // Send mail
+        $this->sendEmail();
         return redirect('inquiry');
     }
 
@@ -43,6 +46,25 @@ class InquiryController extends Controller
         $resultJson = json_decode($result);
         if ($resultJson->success != true) {
             return back()->with('message', 'Captcha error');
+        }
+    }
+
+    public function sendEmail()
+    {
+        $data['title'] = "This is Test Mail Tuts Make";
+
+        Mail::send('emails.new_inquiry', $data, function ($message) {
+
+            $message->to('info@seminari365.com', 'qac-bg.com inquiry')
+
+                ->subject('Tuts Make Mail');
+        });
+
+        if (Mail::failures())
+        {
+            return 'Sorry! Please try again latter';
+        } else {
+            return 'Great! Successfully send in your mail';
         }
     }
 }
